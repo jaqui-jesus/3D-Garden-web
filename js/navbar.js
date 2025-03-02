7ecb7d1672b27461f525791ed3059017fae9ff9a
Yairgg95 <yair.g.guadarrama@icloud.com>
2025-03-01 21:34:55 -0600

    [REFACTOR]Modify logic to always render link to products.html

--- js/navbar.js
+++ js/navbar.js
@@ -12,14 +12,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const homeBtn = document.getElementById("home-btn");
   const managementBtn = document.getElementById("management-btn");
-  const productsBtn = document.getElementById("products-btn");
   const registerBtn = document.getElementById("register-btn");
   const loginBtn = document.getElementById("login-btn");
   const logoutBtn = document.getElementById("logout-btn");
 
     homeBtn.style.display = (isLoggein || isAdminLoggein) ? "none" : "block";
     managementBtn.style.display = isAdminLoggein ? "block" : "none";
-    productsBtn.style.display = (isLoggein || isAdminLoggein) ? "block" : "none";
     registerBtn.style.display = (isLoggein || isAdminLoggein) ? "none" : "block";
     loginBtn.style.display = (isLoggein || isAdminLoggein) ? "none" : "block";
     logoutBtn.style.display = (isLoggein || isAdminLoggein) ? "block" : "none";
@@ -30,10 +28,10 @@ function logout() {
   const isAdminLoggein = localStorage.getItem("adminToken")
   if (isAdminLoggein) {
     localStorage.removeItem("adminToken")
-    window.location.reload();
+    window.open("../", "_self")
   } else {
     localStorage.removeItem("token");
-    window.location.reload()
+    window.open("../", "_self")
   }
 }
 
